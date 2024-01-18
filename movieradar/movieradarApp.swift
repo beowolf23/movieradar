ec77cefe639ea2f161f7c5d9562f30d77c6cf126
Mihai Ciocan <mihaiciocan44@gmail.com>
2024-01-18 23:41:09 +0200

    initialized project

---
+++ movieradar/movieradarApp.swift
@@ -0,0 +1,17 @@
+//
+//  movieradarApp.swift
+//  movieradar
+//
+//  Created by beowolf23 on 18.01.2024.
+//
+
+import SwiftUI
+
+@main
+struct movieradarApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
